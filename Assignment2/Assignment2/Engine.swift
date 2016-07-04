2785cb25b86938533b6a98dc7185d2b4567ee5c5
Jp <jp.lafond@gmail.com>
2016-07-04 13:17:21 -0400

    Jp's Comments on Engine.swift

--- Assignment2/Assignment2/Engine.swift
+++ Assignment2/Assignment2/Engine.swift
@@ -6,14 +6,27 @@
 //  Copyright © 2016 Harvard Summer School. All rights reserved.
 //
 
+/*
+ * NOTES: Jp Comments
+ *  Typically, when you create a new class, good coding style is to have comments at the top of the class to explain how
+ *  it's used, where it's used, etc...
+ *
+ * You'll want to set up the properties, methods, etc... for the classes (which you'll continue to learn about in this
+ * class).
+ */
+
 import Foundation
 
+// NIT: Jp Comment -- This makes the assumption that the grid will be a uniform square. By making that assumption, that could 
+//                    cause problems with row/column confusion but are being hidden, because you have identical sizes for both
+//                    axises.
 // Size of the matrix
 let size = 10
 
 // Shifts to find row and column of neighbouring cells
 let shifts = [-1, 0, 1]
 
+// NIT: Jp Comment -- Good coding style is to make sure that any public method is commented to let a user know how to use it, clarify the parameters, results, etc...
 func step2(before: [[Bool]]) -> [[Bool]] {
     
     // Defining the 'after' cell grid
@@ -32,6 +45,7 @@ func step2(before: [[Bool]]) -> [[Bool]] {
         for column in 0..<size {
             let cellNeighbours = neighbours((row, column))
             let numberOfNeighboursAlive = countNeighboursAlive(before, cellNeighbours: cellNeighbours)
+            // NIT: Jp Comment -- I'd turn this into a state function or something like that that would take a grid, a row/column tuple (the same thing that the neighbors array creates), and returns the state from there (ie; Dead, Starvation, OverPopulation, Alive, Reproduced), as that may turn out to be important in subsequent project assginments.
             switch numberOfNeighboursAlive {
             // Cell state unchanged
             case 2:
@@ -63,6 +77,7 @@ func countNeighboursAlive(cellGrid: [[Bool]], cellNeighbours: [(Int, Int)]) -> I
 }
 
 // Return array of neighbours to a given cell
+// NIT: Jp Comment -- I'd use a defined tuple for these, so that you can use the Swift strong type checking. ie; (row: Int, column: Int) and then that could be typeAlias-ed to be the CellCoordinates. You could then use CellCoordinates in the subsequent arguments, etc...
 func neighbours(cellCoordinates: (Int, Int)) -> [(Int, Int)]{
     var neighbouringCells: [(Int, Int)] = []
     for yShift in shifts {
@@ -72,6 +87,7 @@ func neighbours(cellCoordinates: (Int, Int)) -> [(Int, Int)]{
                 
                 // Handling wrapping for cells along the edges
                 var neighbouringColumn = (cellCoordinates.1 + xShift) % size
+                // NIT: Jp Comment -- Really happy to see this. This is an easy mistake to make.
                 if neighbouringColumn == -1 {
                     neighbouringColumn += size
                 }
